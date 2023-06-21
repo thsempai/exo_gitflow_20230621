f83edf1098fc90dd54b7da448b71380d1b14fcd7
Thomas Stassin <thomas@teampanoptes.be>
2023-06-21 10:19:24 +0200

    say something about age

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 user_name = input("Quel est ton nom? ")
 print(f"Bonjour {user_name}")
 age = input("Quel est ton âge?")
+
+if age < 18:
+    print("Désolé tu ne peux pas être ici.")
+else:
+    print("Bienvenue à Interface 3.")
